786df898ff0fb2104df76763d2c9f956c0599d7b
Marcos Chen <marcostcchen@gmail.com>
2020-07-22 17:43:12 -0300

    App - added spash screen

--- app_iot_garden/src/App.js
+++ app_iot_garden/src/App.js
@@ -3,10 +3,15 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import * as View from './views';
+import SplashScreen from 'react-native-splash-screen';
 
 const Drawer = createDrawerNavigator();
 
 export default class App extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+
   render() {
     return (
       <NavigationContainer>

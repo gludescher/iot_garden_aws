cd89548cc2471812fdcfeb9d117707f513b4e6df
Marcos Chen <marcostcchen@gmail.com>
2020-07-24 08:52:50 -0300

    App - removed app

--- app_iot_garden/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './constants';
-export * from './mockedFetch';
-export * from './utils';
--- app_iot_garden/src/views/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './HomeScreen';
-export * from './MinhasPlantasScreen';
-export * from './LoginScreen/LoginScreen';
